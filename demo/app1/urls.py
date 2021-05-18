d7b772dbeef286f42964261c5470747f2bbe79fc
xinyue1007 <851910295@qq.com>
2021-05-18 13:44:20 +0800

    add base html and topics

--- demo/app1/urls.py
+++ demo/app1/urls.py
@@ -1,8 +1,12 @@
 
 from . import views as app1_views
 from django.conf.urls import url
-
+app_name = 'app1'
 urlpatterns = [
       #主页
       url(r'^$', app1_views.index, name='index'),
+      #显示所有主题
+      url(r'^topics/$', app1_views.topics, name='topics'),
+      #特定主题的详细页面w
+      url(r'^topics/(?P<topic_id>\d+)/$', app1_views.topic, name='topic'),
 ]
--- demo/app1/views.py
+++ demo/app1/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Topic
+
+
 def index(request):
     """学习笔记的主页"""
     return render(request,'app1/index.html')
+
+def topics(request):
+    """显示所有主题"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics':topics}
+    return render(request,'app1/topics.html',context)
+
+def topic(request, topic_id):
+    """显示单个主题及其所有的条目"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'app1/topic.html', context)
